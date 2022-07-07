e137ee3eaff7f1f3fd28408a47d1f0b8e6c91816
Travis Levi B <tbateswsu@gmail.com>
2022-07-07 16:02:56 -0700

    feat: added plugins and modules for webpack to webpack.config. Added updated favicon and logo images.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Text Editor Progressive Web App
+
+# Description
+
+This simple text editor is a progressive web app that uses the browser service workers feature and webpack to provide an experiance similar to native apps. The application can be run without an internet application or installed and access from an icon on the desktop.
+
+# Installation
+
+To install, edit list
+
+Install Node: [Node.js](https://nodejs.org/en/download/).
+
+Install Express: [Express.js](https://www.npmjs.com/package/express).
+
+Install Webpack: []().
+
+# Appearance
+
+Edit me
+
+![Appearance](./public/images/)
+
+# User Story
+
+AS A developer I WANT to create notes or code snippets with or without an internet connection SO THAT I can reliably retrieve them for later use
+
+# Usage
+
+Edit me
+
+## Credits
+
+Credits to Node.js, Express,js, Webpack, Heroku,
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,77 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "PWA Text Editor",
+        short_name: "JATE",
+        description: "Progressive web app text editor.",
+        // select a background color #000000
+        background_color: "#141C3A",
+        // select a theme color #000000
+        theme_color: "#141C3A",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            // fill in the name of the icon image file(s)
+            src: path.resolve("src/images/name.png"),
+            // fill in the image sizes.
+            sizes: [],
+            distination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          // look for files that end in .css
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          // look for files that end in .js / but exclude node_modules
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
