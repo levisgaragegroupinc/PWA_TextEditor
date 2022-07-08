2552a155a0c8b078ab84404bae764d0e8a36d8af
Travis Levi B <tbateswsu@gmail.com>
2022-07-07 17:00:19 -0700

    feat: added click event handler for butInstall element.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,7 +8,15 @@ window.addEventListener("beforeinstallprompt", (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {});
+butInstall.addEventListener("click", async () => {
+  const elementEvent = window.deferredPrompt;
+  if (!elementEvent) {
+    return;
+  }
+  elementEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {});
