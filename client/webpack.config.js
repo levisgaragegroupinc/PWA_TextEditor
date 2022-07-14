909d6655f9f3e6daa67010109a455e1a78ed9dbb
Travis Bates <tbateswsu@gmail.com>
2022-07-13 21:49:57 -0700

    src for favicon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,6 +39,11 @@ module.exports = () => {
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("assets", "icons"),
           },
+          {
+            src: path.resolve("./favicon.ico"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
         ],
       }),
     ],
